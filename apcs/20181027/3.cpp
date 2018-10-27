b21084e1f9d599183893e66716014a79e728cb53
maple3142 <kirby741852963@gmail.com>
2018-10-27 20:42:29 +0800

    add comments

--- apcs/20181027/3.cpp
+++ apcs/20181027/3.cpp
@@ -3,33 +3,52 @@
 using namespace std;
 typedef pair<int, int> pii;
 
+/*
+ * Q:
+ * 輸入整數 n 與字串 s，n 代表以 2^n 為邊長的正方形，s 是正方形的排列
+ * 如 s = 2200101020011 代表: (2 代表分隔為 4 塊，接著的 4 個數字依序代表 4 個區塊的模樣)
+ * 0011
+ * 0111
+ * 0000
+ * 0011
+*/
+
 int qpow(int a, int b) {
-	if (b == 1)
-		return a;
+	/*
+	 * 快速冪
+	 * if(n % 2 == 0) a^b = a^(b/2) * a^(b/2)
+	 * else a^b = a^(b/2) * a^(b/2) * a
+	*/
+	if (b == 0)
+		return 1;
 	int x = qpow(a, b / 2);
 	return b % 2 == 0 ? x * x : x * x * a;
 }
-int n, pn;
+int n;
 string s;
-pii dfs(int cur, int deep = 0) {
+pii dfs(int start, int deep = 0) {
 	int cnt = 0;
-	int i = cur;
+	int cursor = start;
 	for (int j = 0; j < 4; j++) {
-		char c = s[i] - '0';
+		int c = s[cursor] - '0';
+		if (deep == 0 & j > 0) // 第 0 層第一個數字如果不是 2 根本不會有後面的出現
+			break;
 		if (c == 2) {
-			pii x = dfs(i + 1, deep + 1);
+			// 如果是 2，則把後面的交給下一層來判斷
+			pii x = dfs(cursor + 1, deep + 1);
+			// 然後加上傳回來的 cnt 和 curson 移動的距離
 			cnt += x.first;
-			i += x.second;
+			cursor += x.second;
 		} else if (c == 1) {
-			cnt += pn / qpow(4, deep);
+			// 是 1 就加上本層每格的格子數量 (2^2n) / (4^m) = 4^(n - m)
+			cnt += qpow(4, n - deep);
 		}
-		i++;
+		cursor++;
 	}
-	return pii(cnt, i - cur);
+	return pii(cnt, cursor - start); // 回傳 cnt 與 cursor 移動的距離
 }
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin >> n >> s;
-	pn = qpow(2, 2 * n);
 	cout << dfs(0).first << endl;
 }
